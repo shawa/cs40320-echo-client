df86c364c1a62935d044a486ac7038a6666b1926
shawa <andrewshaw.mail@gmail.com>
2016-11-01 16:13:49 +0000

    add env auto create

--- run.sh
+++ run.sh
@@ -1,1 +1,8 @@
-./env/bin/python3 client.py $@
+#!/bin/bash
+if [ -d env ]; then
+    ./env/bin/python3 client.py $@
+else
+    echo "Virtualenv doesn't seem to exist, creating..."
+    ./createvirtualenv.sh
+    ./env/bin/python3 client.py -h
+fi
